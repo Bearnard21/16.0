a50f244412c45d4af02a27877e62f08070a06643
Mikhail Lapin <115989247+Bearnard21@users.noreply.github.com>
2022-12-01 14:05:23 +0300

    Add files via upload

--- cx_website_cetmix/__manifest__.py
+++ cx_website_cetmix/__manifest__.py
@@ -8,19 +8,23 @@
     'website': 'https://cetmix.com',
     'live_test_url': 'https://demo.cetmix.com',
     "depends": [
-        "base",
-        "mail",
         "website",
         "web_unsplash",
         'website_profile',
     ],
-    'external_dependencies': {
-    },
+    'external_dependencies': {},
     "data": [
-        'data/ir_asset.xml',
+        #"views/cx_header_logo.xml",
         "views/cx_header.xml",
         "views/cx_homepage.xml",
     ],
+    "assets": {
+        'web.assets_frontend': [
+            "cx_website_cetmix/static/src/scss/user_values.scss",
+            #"cx_website_cetmix/static/src/scss/colors/user_color_palette.scss",
+            #"cx_website_cetmix/static/src/scss/colors/user_theme_color_palette.scss",
+        ],
+    },
     "qweb": [],
     "installable": True,
     "application": True,
--- cx_website_cetmix/controllers/cx_website.py
+++ cx_website_cetmix/controllers/cx_website.py
@@ -4,6 +4,7 @@ from odoo.http import request
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class CetmixWebsiteTemplate(http.Controller):
     @http.route("/cetmix/home", type="http", auth="public", website=True)
     def cetmix_website_template(self):
